f6b0182bd83e5826aa57389ae06ba50a386c71f1
Bryan Leanoe <bryan.leano@gmail.com>
2018-12-25 01:01:47 -0700

    add assign grade for loop example

--- script.js
+++ script.js
@@ -40,7 +40,27 @@ var i;
 for (i = 0; i <= 10; i++) {
 	console.log(i * 9);
 };
- 
+
+// Check the results of your assignGrade function from the conditionals exercise by logging every value from 60 to 100: your log should show "For 88, you got a B. For 89, you got a B. For 90, you got an A. For 91, you got an A.", etc., logging each grade point in the range.
+
+function assignGrade(score) {
+	if (score > 90) {
+			return 'A';
+	} else if (score > 80) {
+			return 'B';
+	} else if (score > 70) {
+			return 'C';
+	} else if (score > 65) {
+			return 'D';
+	} else {
+			return 'F';
+	}
+}
+
+for (var i = 60; i <= 100; i++) {
+console.log('For ' + i + ', you got a ' + assignGrade(i));
+}
+
 
 /**
 * While Loop
