eb922b422e72e4b970578852988ab7b1b71004c1
Bryan Leanoe <bryan.leano@gmail.com>
2018-12-25 00:38:22 -0700

    add multiplication table for loop example

--- script.js
+++ script.js
@@ -23,13 +23,22 @@ for (i = 0; i < myArray.length; i++) {
 
 // Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
 
-var i = 0;
+var i;
 for (i = 0; i <= 20; i++) {
-	if (i & 1) { 
-		console.log(i + ' is odd');
-	} else {
+	if (i % 2 === 0) { 
 		console.log(i + ' is even');
+	} else {
+		console.log(i + ' is odd');
 	}
+};
+
+// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+
+// Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+var i;
+for (i = 0; i <= 10; i++) {
+	console.log(i * 9);
 };
  
 
